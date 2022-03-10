e257ecfe3be555517427c81af0f215cbfba98d9c
Ramos Fonseca <beidseitig@gmail.com>
2022-03-10 03:44:45 -0300

    Requisito 5 - Criando o formulario do search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { searchInput } = this.state;
+    const min = 2;
+
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          placeholder="Search..."
+          name="searchInput"
+          value={ searchInput }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          name="searchButton"
+          disabled={ searchInput.length < min }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
